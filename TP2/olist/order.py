0ffc0c6d8245dc104d83aa399f5e5923223c8c3d
CamilaVSobrino <Cvsobrino24@ucema.edu.ar>
2023-11-01 13:02:50 -0300

    cuarto

--- TP2/olist/order.py
+++ TP2/olist/order.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 from olist.load_data import load_all_data
 data = load_all_data("./data")
 
-# ORDERS
 from olist.preprocess import transformar_columnas_datetime
 from olist.preprocess import tiempo_de_espera
 from olist.preprocess import real_vs_esperado
@@ -29,7 +28,6 @@ orders.drop('order_estimated_delivery_date', axis=1, inplace=True)
 
 orders.rename(columns={'order_status': 'status_de_la_orden'}, inplace=True)
 
-# REVIEWS
 from olist.preprocess import puntaje_de_compra
 
 reviews = data['order_reviews'].copy()
@@ -37,26 +35,21 @@ reviews = reviews[["order_id", "review_score"]]
 
 reviews = puntaje_de_compra(reviews)
 
-# NUMERO_DE_PRODUCTOS
 from olist.preprocess import calcular_numero_productos
 
 numero_de_productos = data['order_items'].copy()
 numero_de_productos = calcular_numero_productos(numero_de_productos)
 
-
-# NUMERO_DE_VENDEDORES
 from olist.preprocess import numero_de_vendedores
 
 order_items = data['order_items'].copy()
 numero_de_vendedores = numero_de_vendedores(order_items)
 
-# CALCULAR_PRECIO_Y_TRANSPORTE
 from olist.preprocess import calcular_precio_y_transporte
 
 order_items = data['order_items'].copy()
 precio_y_transporte = calcular_precio_y_transporte(order_items)
 
-# DISTANCIAS
 from olist.preprocess import haversine_distance
 from olist.preprocess import calcular_distancia_vendedor_comprador
 
